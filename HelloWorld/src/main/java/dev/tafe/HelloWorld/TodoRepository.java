aa3e697af4da263b262d0ac3194c29546f81ba7b
TAWFEEQ BAHURUDEEN <tawfeeqbahur@gmail.com>
2026-05-15 07:48:46 +0530

    Delete HelloWorld directory

--- HelloWorld/src/main/java/dev/tafe/HelloWorld/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.tafe.HelloWorld;
-
-import dev.tafe.HelloWorld.model.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
